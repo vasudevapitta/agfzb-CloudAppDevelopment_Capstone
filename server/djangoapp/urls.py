2393a8c693a40652c28950fba90526c4da7d846d
vasudeva pitta <tizzicboy@gmail.com>
2022-06-21 22:33:19 +0000

    add static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about/', view=views.get_about, name='about'),
 
     # path for contact us view
+    path(route='contact/', view=views.get_contact, name='contact'),
 
     # path for registration
 
