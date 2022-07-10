fce286089c4321fdd686e58655218cafd5c61f53
Vasudeva Pitta <tizzicboy@gmail.com>
2022-07-10 17:06:21 +0000

    create ibm cloud function endpoints

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -76,17 +76,91 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    print("Index Request")
     context = {}
     if request.method == "GET":
+        url = "https://630f2802-ef1e-4b85-80eb-a1d6f6e2644b-bluemix.cloudantnosqldb.appdomain.cloud/api/dealership?state=\"\""
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        context['dealership_list'] = dealerships
+        # Return a list of dealer short name
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealer_details(request, dealer_id):
+
+    if request.method == "GET":
+        context=dict()
+        url = "https://630f2802-ef1e-4b85-80eb-a1d6f6e2644b-bluemix.cloudantnosqldb.appdomain.cloud"
+        dealer_url = url + "/api/dealership"
+        review_url = url + "/api/review"
+
+        dealer, dealer_status = get_dealer_by_id(dealer_url, dealer_id)
+        context["dealer"] = dealer
+        context["dealer_result"] = dealer_status
+
+        reviews, review_status = get_dealer_reviews_from_cf(review_url, dealer_id)
+        context["reviews"]=reviews
+        context["review_result"]=review_status
+
+        context["dealer_id"] = dealer_id
+
+        return render(request, 'djangoapp/dealer_details.html', context)
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+def add_review(request, dealer_id):
+    if request.method == "GET":
+        context = dict()
+        url = "https://630f2802-ef1e-4b85-80eb-a1d6f6e2644b-bluemix.cloudantnosqldb.appdomain.cloud"
 
+        dealer, dealer_status = get_dealer_by_id(url, dealer_id)
+        context["dealer"] = dealer
+        context["result"] = dealer_status
+        cars = CarModel.objects.all().filter(dealer_id=dealer_id)
+        context["cars"] = cars
+        context["dealer_id"] = dealer_id
+        return render(request, "djangoapp/add_review.html", context)
+
+    elif request.method == "POST":
+        if request.user.is_authenticated:
+            url = "https://630f2802-ef1e-4b85-80eb-a1d6f6e2644b-bluemix.cloudantnosqldb.appdomain.cloud"
+            review = dict()
+            review["name"] = request.user.username
+            review["dealership"] = dealer_id
+            review["review"] = request.POST["content"]
+            review["purchase"] = True if (request.POST.get('purchasecheck', "False") == "on") else False
+            car_id = request.POST.get("car", None)
+            if car_id != None:
+                car = CarModel.objects.get(id=car_id)
+                review["car_make"] = car.car_make.name
+                review["car_model"] = car.name
+                review["car_year"] = int(car.yearpublished())
+                review["purchase_date"] = request.POST["purchasedate"]
+                     
+            json_payload = dict()
+            json_payload["review"] = review
+            result, status_code = post_request(url, json_payload, dealer_id = dealer_id)
+            print(result)
+
+            if (result.get('message') == "ok"):
+                return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+            else:
+                context=dict()
+                context["error"] = result["message"]
+                return render(request, 'djangoapp/add_review.html', context)
+        else:    
+            context=dict()
+            context["error"] = "User is not authenticated"
+            url = "https://630f2802-ef1e-4b85-80eb-a1d6f6e2644b-bluemix.cloudantnosqldb.appdomain.cloud"
+            dealer, dealer_status = get_dealer_by_id(url, dealer_id)
+            context["dealer"] = dealer
+            context["result"] = dealer_status
+            context["dealer_id"] = dealer_id
+            cars = CarModel.objects.all().filter(dealer_id = dealer_id)
+            
+            return render(request, "djangoapp/add_review.html", context)
